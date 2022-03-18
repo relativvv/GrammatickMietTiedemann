d8f2aced34ddecbb312ae4824a234258d401a713
Robin Schulte <r.schulte@shopware.com>
2022-03-18 13:49:03 +0100

    Add Category Entity

---
+++ backend/src/Controller/CategoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\CategoryService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    public function __construct(private CategoryService $categoryService)
+    {
+    }
+
+    #[Route('/category/all', name: 'app_category')]
+    public function getAllCategories(): JsonResponse
+    {
+        $categories = array_map(static fn ($category) => $category->toArray(), $this->categoryService->getAllCategories());
+
+        return new JsonResponse($categories);
+    }
+}
