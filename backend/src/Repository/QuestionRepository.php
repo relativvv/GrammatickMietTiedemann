87b2060b6942302bac073581aaddb99e0aab7762
Robin Schulte <r.schulte@shopware.com>
2022-04-01 14:17:23 +0200

    Add wordselect entity

---
+++ backend/src/Repository/QuestionRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Question;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+class QuestionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Question::class);
+    }
+
+    public function findQuestionById(int $id)
+    {
+        return $this->findOneBy(['id' => $id]);
+    }
+}
