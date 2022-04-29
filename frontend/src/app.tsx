597508429158bceb5bc284e9c3fa6f03dfa13e3e
Noah <noah.dahlhaus@tobit.com>
2022-04-29 14:42:03 +0200

    Add api data

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,11 +1,23 @@
-import React, {FC} from 'react';
+import React, { FC, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.scss';
 import Header from "./components/header/header";
 import Description from "./components/description/description";
 import {Container} from "react-bootstrap";
+import { getAllCategories } from './api/categories/categories';
 
 const app: FC<Record<string, never>> = () => {
+
+    const fetchCategories = async () => {
+        const cat = await getAllCategories();
+        console.log("cat");
+    }
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        fetchCategories().then();
+    }, []);
+
     return (
         <>
             <Header />
