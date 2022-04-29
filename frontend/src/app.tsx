c776f60a5fad350227c0efb562567aa4988a8ad4
Mona Uppenkamp <mona.uppenkamp@tobit.com>
2022-04-29 13:29:35 +0200

    :sparkles: Add description

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,11 +2,16 @@ import React, {FC} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.scss';
 import Header from "./components/header/header";
+import Description from "./components/description/description";
+import {Container} from "react-bootstrap";
 
 const app: FC<Record<string, never>> = () => {
     return (
         <>
             <Header />
+            <Container className='container'>
+                <Description />
+            </Container>
         </>
     )
 };
---
+++ frontend/src/components/description/description.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Description = () => (
+    <div>
+        <h3>Willkommen auf dem Grammatik-Portal der ITM1!</h3>
+        <p>Hier findet Ihr einen Überblick über jegliche Themen, die
+            man so in der Schule brauchen könntet. Zu jeder Erklärung findet Ihr auch eine oder mehrere Übungsaufgaben.</p>
+        <p>Viel Spaß!</p>
+    </div>
+);
+
+export default Description;
