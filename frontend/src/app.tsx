dfb19fa33fa1d97b498acad8e424babf827bff3c
Mona Uppenkamp <mona.uppenkamp@tobit.com>
2022-04-29 14:42:55 +0200

    :sparkles: Add isFrontPage

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.scss';
 import Header from "./components/header/header";
@@ -6,18 +6,23 @@ import Description from "./components/description/description";
 import {Container} from "react-bootstrap";
 import Topic from "./components/topic/topic";
 
-const app: FC<Record<string, never>> = () => {
+const app: React.FunctionComponent = () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [isFrontPage, setIsFrontPage] = useState(true);
+
     return (
         <>
             <Header />
             <Container className='container'>
                 <Description />
                 <Topic
-                    frontPage
+                    frontPage={isFrontPage}
                     image='https://www.kapiert.de/media/image/AB1FDD01/A4ED779D/79386D4A/3B422604.png'
                     alt='dass/das Bild'
                     headline='Dass oder das?'
                     description={'In jedem Fall bezieht sich ein “das” auf ein Nomen und lässt sich durch “dieses”, “jenes” oder “welches” ersetzen. \n Da sich das “dass” auf kein Nomen bezieht, sondern als Konjunktion einen Nebensatz einleitet, lässt es sich durch kein anderes Wort ersetzen. \n In jedem Fall bezieht sich ein “das” auf ein Nomen und lässt sich durch “dieses”, “jenes” oder “welches” ersetzen. Da sich das “dass” auf kein Nomen bezieht, sondern als Konjunktion einen Nebensatz einleitet, lässt es sich durch kein anderes Wort ersetzen.'}
+                    // @ts-expect-error
+                    setIsFrontPage={setIsFrontPage}
                 />
             </Container>
         </>
--- frontend/src/components/topic/topic.tsx
+++ frontend/src/components/topic/topic.tsx
@@ -7,19 +7,25 @@ type TopicProps = {
     image: string,
     alt: string,
     headline: string,
-    description: string
+    description: string,
+    setIsFrontPage: () => {}
 };
 
 const Topic: React.FunctionComponent<TopicProps> = ({
-    frontPage,
-    image,
-    alt,
-    headline,
-    description
+                                                        frontPage,
+                                                        image,
+                                                        alt,
+                                                        headline,
+                                                        description,
+                                                        setIsFrontPage
                                                     }) => {
     if (frontPage) {
         return (
-            <Container className='d-flex flex-row topic-front-page'>
+            <Container
+                className='d-flex flex-row topic-front-page'
+                // @ts-expect-error
+                onClick={() => setIsFrontPage(false)}
+            >
                 <div className='p2 topic-front-page__text'>
                     <h1>{headline}</h1>
                     <p dangerouslySetInnerHTML={{
@@ -38,6 +44,14 @@ const Topic: React.FunctionComponent<TopicProps> = ({
 
     return (
         <>
+            <Container className='topic'>
+                <Image
+                    className='topic__image'
+                    src={image}
+                    alt={alt}
+                    thumbnail
+                />
+            </Container>
         </>
     );
 };
