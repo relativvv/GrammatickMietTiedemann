e29da6452ad94c9f13fc08d712d927d042396668
Mona Uppenkamp <mona.uppenkamp@tobit.com>
2022-05-13 14:16:18 +0200

    :sparkles: Map topics

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -7,16 +7,31 @@ import Description from "./components/description/description";
 import {Container} from "react-bootstrap";
 import MultipleChoice from "./components/answerTypes/multipleChoice/multipleChoice";
 import { getAllCategories, getQuestions } from './api/categories/categories';
+import {Category} from "./types/categories";
+import Topic from "./components/topic/topic";
 
 const app: React.FunctionComponent = () => {
-    const [topics, setTopics] = useState([1, 2, 3]);
+    const [topics, setTopics] = useState([{
+        id: 1,
+        name: 'Dass oder das?',
+        description: 'In jedem Fall bezieht sich ein “das” auf ein Nomen und lässt sich durch “dieses”, “jenes” oder “welches” ersetzen. \n Da sich das “dass” auf kein Nomen bezieht, sondern als Konjunktion einen Nebensatz einleitet, lässt es sich durch kein anderes Wort ersetzen. \n In jedem Fall bezieht sich ein “das” auf ein Nomen und lässt sich durch “dieses”, “jenes” oder “welches” ersetzen. Da sich das “dass” auf kein Nomen bezieht, sondern als Konjunktion einen Nebensatz einleitet, lässt es sich durch kein anderes Wort ersetzen.'
+    },
+    {
+        id: 2,
+        name: 'Rechtschreibung',
+        description: 'In jedem Fall bezieht sich ein “das” auf ein Nomen und lässt sich durch “dieses”, “jenes” oder “welches” ersetzen. \n Da sich das “dass” auf kein Nomen bezieht, sondern als Konjunktion einen Nebensatz einleitet, lässt es sich durch kein anderes Wort ersetzen. \n In jedem Fall bezieht sich ein “das” auf ein Nomen und lässt sich durch “dieses”, “jenes” oder “welches” ersetzen. Da sich das “dass” auf kein Nomen bezieht, sondern als Konjunktion einen Nebensatz einleitet, lässt es sich durch kein anderes Wort ersetzen.'
+    },
+    {
+        id: 3,
+        name: 'Groß- oder Kleinschreibung',
+        description: 'In jedem Fall bezieht sich ein “das” auf ein Nomen und lässt sich durch “dieses”, “jenes” oder “welches” ersetzen. \n Da sich das “dass” auf kein Nomen bezieht, sondern als Konjunktion einen Nebensatz einleitet, lässt es sich durch kein anderes Wort ersetzen. \n In jedem Fall bezieht sich ein “das” auf ein Nomen und lässt sich durch “dieses”, “jenes” oder “welches” ersetzen. Da sich das “dass” auf kein Nomen bezieht, sondern als Konjunktion einen Nebensatz einleitet, lässt es sich durch kein anderes Wort ersetzen.'
+    }] as Array<Category>);
     const [selectedTopicId, setSelectedTopicId] = useState(0);
-    const [selectedTopic, setSelectedTopic] = useState(null);
 
     const fetchCategories = async () => {
-        const cat = await getAllCategories();
-        console.log("cat", cat);
-        const test = await getQuestions(cat[0].id);
+        const categories = await getAllCategories();
+        setTopics(categories);
+        const test = await getQuestions(categories[0].id);
         console.log("test", test);
     }
 
@@ -37,12 +52,12 @@ const app: React.FunctionComponent = () => {
                                 return (
                                     <Topic
                                         key={index}
-                                        id={index+1}
+                                        id={t.id}
                                         frontPage={selectedTopicId === 0}
                                         image='https://www.kapiert.de/media/image/AB1FDD01/A4ED779D/79386D4A/3B422604.png'
                                         alt='dass/das Bild'
-                                        headline='Dass oder das?'
-                                        description={'In jedem Fall bezieht sich ein “das” auf ein Nomen und lässt sich durch “dieses”, “jenes” oder “welches” ersetzen. \n Da sich das “dass” auf kein Nomen bezieht, sondern als Konjunktion einen Nebensatz einleitet, lässt es sich durch kein anderes Wort ersetzen. \n In jedem Fall bezieht sich ein “das” auf ein Nomen und lässt sich durch “dieses”, “jenes” oder “welches” ersetzen. Da sich das “dass” auf kein Nomen bezieht, sondern als Konjunktion einen Nebensatz einleitet, lässt es sich durch kein anderes Wort ersetzen.'}
+                                        headline={t.name}
+                                        description={t.description}
                                         // @ts-expect-error
                                         setIsFrontPage={setSelectedTopicId}
                                     />
