6dbeb5779e1f001cecd96766a695d4d41b9ab127
Noah <noah.dahlhaus@tobit.com>
2022-05-13 14:07:28 +0200

    Add types

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -4,14 +4,20 @@ import './app.scss';
 import Header from "./components/header/header";
 import Description from "./components/description/description";
 import {Container} from "react-bootstrap";
+<<<<<<< Updated upstream
 import MultipleChoice from "./components/answerTypes/multipleChoice/multipleChoice";
 import { getAllCategories } from './api/categories/categories';
+=======
+import { getAllCategories, getQuestions } from './api/categories/categories';
+>>>>>>> Stashed changes
 
 const app: FC<Record<string, never>> = () => {
 
     const fetchCategories = async () => {
         const cat = await getAllCategories();
-        console.log("cat");
+        console.log("cat", cat);
+        const test = await getQuestions(cat[0].id);
+        console.log("test", test);
     }
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
